5b9fe9616963d29f22e9df475e5ccca52ce9dbc3
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-05 16:43:58 +0100

    lib.rs with run(), question(), Modifications and banner_and_clear_screen()

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+pub mod directory_scanner;
+pub mod image_modifier;
+
+use colored::*;
+
+pub struct Modifications {
+    resize: f32,
+}
+
+impl Default for Modifications {
+    fn default() -> Self {
+        Modifications { resize: 1.0 }
+    }
+}
+
+pub fn run(imgs: &Vec<String>) {
+    banner_and_clear_screen();
+
+    if question("Do you want to work on all images? (Y/n)", "n") {
+        if question("Do you want to apply modifications? (y/N)", "y") {
+            let modifications = Modifications {
+                ..Default::default()
+            };
+            for filename in imgs {
+                println!("Processing {}", filename);
+                if let Err(e) = image_modifier::modify_image(&filename, &modifications) {
+                    println!("{} | Unable to modify {}", "Fail".red().bold(), filename);
+                    println!("{}: {}", "Error".red(), e)
+                } else {
+                    println!("{} | Modified {}", "Success".green().bold(), filename);
+                }
+            }
+        } else {
+            println!("Currently unavailable.");
+        }
+    } else {
+        println!("Currently unavailable.");
+    }
+}
+
+fn question(content: &str, nondefault: &str) -> bool {
+    println!("{}", content);
+
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+
+    let first_character = line.chars().nth(0).unwrap().to_lowercase().to_string();
+
+    banner_and_clear_screen();
+
+    !(first_character == nondefault)
+}
+
+fn banner_and_clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
+    println!("{}{}", "img".green().bold(), "mng".cyan().bold());
+}
