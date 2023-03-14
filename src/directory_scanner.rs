c44ad6d602568f2b2e680a6eeb57a3c98d89d909
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-14 17:43:44 +0100

    list of accepted image formats

--- src/directory_scanner.rs
+++ src/directory_scanner.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+const ACCEPTED_FILE_FORMATS: [&str; 3] = ["jpeg", "jpg", "png"];
+
 pub fn find_images(path: &str) -> Vec<String> {
     let paths = fs::read_dir(path).unwrap();
     let mut res = Vec::new();
@@ -7,10 +9,20 @@ pub fn find_images(path: &str) -> Vec<String> {
     for path in paths {
         let filename = path.unwrap().file_name();
         let string_filename = filename.to_str().unwrap();
-        if str::ends_with(string_filename, ".jpg") {
+        if is_acceptable_format(string_filename) {
             res.push(String::from(string_filename));
         }
     }
 
     res
+}
+
+fn is_acceptable_format(filename: &str) -> bool {
+    for format in ACCEPTED_FILE_FORMATS {
+        if str::ends_with(filename, format) {
+            return true;
+        }
+    }
+
+    false
 }
